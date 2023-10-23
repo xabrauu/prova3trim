4ad27688cbfcee15441ebee7ae7dbfa81d771dd3
xabrauu <130366440+xabrauu@users.noreply.github.com>
2023-10-23 11:35:21 -0300

    Add files via upload
    não consigo

---
+++ prova front/Calcular.js
@@ -0,0 +1,9 @@
+function Calcular() {
+
+    let idadeCanina = prompt("Qual a idade do cachorrinho?");
+    
+
+    mostraResultado.textContent = "Resultado: a idade humana do cachorrinho é " + idadeCanina;
+
+
+}
